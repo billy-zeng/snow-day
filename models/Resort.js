67a0dd914d1c7e35f23dde2bb9789ea38d915883
billy-zeng <bzeng95@gmail.com>
2020-01-30 11:27:08 -0800

    sets up Resort resource schema and model

--- models/Resort.js
+++ models/Resort.js
@@ -1,3 +1,22 @@
+const mongoose = require('mongoose');
 
+const ResortSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  lat: Number,
+  lng: Number,
+  elevation_base: Number,
+  elevation_summit: Number,
+  lifts: Number,
+  runs: Number,
+  mainWebsite: String,
+  ticketWebsite: String,
+  phoneNumber: String,
+  images: [String],
+  reviews: [Review.schema],
+  // favCount: Number,
+});
+
+const Resort = mongoose.model('Resort', ResortSchema);
 
 module.exports = Resort;
