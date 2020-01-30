cc68663566677e90aceea0217132a8e7f0d2a034
rerueter <rerueter@gmail.com>
2020-01-30 11:29:55 -0800

    adds user model and user api route

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.use(bodyParser.json());
 
 //————————————————————————— API Routes ——————————————————————————//
 
+app.get("/api/v1/Users", (req, res) => {
+  DB.User.find({}, (err, response) => {
+    if (err) {
+      return response
+        .status(400)
+        .json({ message: "something went wrong!", err: err });
+    }
+    response.status(200).json({ foundUsers });
+  });
+});
+
 //———————————————————————— Server Start —————————————————————————//
 app.listen(PORT, () => {
   console.log(`Server is listening at ${PORT}.`);
